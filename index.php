f4d8b37e80e8f26e7f94a5d290c12f65fdd19278
FedericoCapitani <federico.capitani@outlook.it>
2022-05-19 15:45:15 +0200

    add new text to censor words

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+$title = "Brutte parole";
+$text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis nemo nam nisi libero optio laboriosam? Error, ipsa non? Veritatis, a quia incidunt velit 
+    quos rerum voluptatem doloribus obcaecati? Saepe, dolorem est recusandae labore rem mollitia deserunt ullam sed distinctio maxime.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, voluptatum! Eligendi dolor, debitis sint reiciendis, possimus nisi quod unde soluta consequatur dolore, at vitae corrupti ullam excepturi earum blanditiis maiores tempore nesciunt ipsum! Repellendus odit consectetur, maiores provident distinctio libero.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit sed a, ut blanditiis deleniti distinctio debitis. Aspernatur delectus quod dignissimos consequatur impedit! Laborum quo nihil quod id enim illum minus itaque, eos ad expedita consectetur assumenda laudantium, dolor in reiciendis?";
+$_GET["word"];
+var_dump($_GET["word"]);
+$new_text = str_replace($_GET["word"], '***', $text);
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +19,9 @@
 </head>
 <body>
     
+    <h1><?php echo $title;?></h1>
+    <p><?php echo $text;?></p>
+    <p><?php echo $new_text;?></p>
     
 </body>
 </html>
